0c8011afdecc96a553f92c177871d68e7d4efcdb
franclin1 <105646543+franclin1@users.noreply.github.com>
2022-07-18 15:24:13 +0200

    Update infrastructure/setting_up_state_s3.sh
    Co-authored-by: T-Kutzner <98714453+T-Kutzner@users.noreply.github.com>

--- infrastructure/setting_up_state_s3.sh
+++ infrastructure/setting_up_state_s3.sh
@@ -4,5 +4,5 @@ if aws s3 ls "s3://$TERRAFORM_S3_BUCKET_NAME" 2>&1 | grep -q 'An error occurred'
 then
     aws s3api create-bucket --bucket $TERRAFORM_S3_BUCKET_NAME --region eu-central-1 --create-bucket-configuration LocationConstraint=eu-central-1
 else
-    echo "Terraform bucket to store state file exists allready"
+    echo "Terraform bucket to store state file exists already"
 fi
