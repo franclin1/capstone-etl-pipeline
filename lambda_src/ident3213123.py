ffc33531542745bc5f73e6e32091e33d45d51dc7
Malte Ahlers <ahlers.malte95@gmail.com>
2022-07-20 20:30:48 +0200

    getting data from receipt/object in s3

--- lambda_src/ident3213123.py
+++ lambda_src/ident3213123.py
@@ -1,18 +1,35 @@
 import boto3
 
+
 s3 = boto3.resource("s3")
-
-#s3_request_file_name = "1.jpg"
+textract = boto3.client("textract")
 s3_source_bucket_name = "tests31tests31"
-
 s3_bucket = s3.Bucket(s3_source_bucket_name)
+
 
 # getting name from image
 def fetch_file_name(s3_bucket):
     for receipt in s3_bucket.objects.all():
-        print(receipt.key)
+        file_name = receipt.key
+    return file_name
+
 
 # pulling data from image
+
+def fetch_data_from_file(file_name):
+    file_name = file_name
+ 
+    response = textract.analyze_expense(
+        Document={
+            'S3Object': {
+                'Bucket': s3_source_bucket_name,
+                'Name': file_name
+            }
+        })
+        
+    print(response)
+   
+
 # passing data to dynamodb
 # deleting image
 
@@ -20,4 +37,6 @@ def fetch_file_name(s3_bucket):
 
 
 ####main###
-fetch_file_name(s3_source_bucket_name)
+
+file_name = fetch_file_name(s3_bucket)
+fetch_data_from_file(file_name)
